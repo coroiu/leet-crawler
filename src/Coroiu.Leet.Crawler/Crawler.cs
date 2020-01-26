b549f4e60ef9347369904bbe25d5178eb109440d
Andreas Coroiu <andreas.coroiu@gmail.com>
2020-01-26 21:42:26 +0100

    Implement crawler.

---
+++ src/Coroiu.Leet.Crawler/Crawler.cs
@@ -0,0 +1,18 @@
+﻿using Coroiu.Leet.Crawler.Net;
+using Coroiu.Leet.Crawler.Storage;
+using System;
+
+namespace Coroiu.Leet.Crawler
+{
+    public class Crawler
+    {
+        public Crawler()
+        {
+        }
+
+        public ICrawlSession CreateSession(Uri startUri, IBrowser browser, IStorage storage)
+        {
+            return new CrawlSession(startUri, browser, storage);
+        }
+    }
+}
