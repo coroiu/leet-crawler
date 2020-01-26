4fa961518beb57ac8aa148d6875a5a85d54fe959
Andreas Coroiu <andreas.coroiu@gmail.com>
2020-01-26 14:15:37 +0100

    Prepare for testing.

---
+++ Coroiu.Leet.Crawler.Test/CrawlSessionTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace Coroiu.Leet.Crawler.Test
+{
+    public class CrawlSessionTest
+    {
+        private ICrawlSession _crawlSession;
+
+        public CrawlSessionTest()
+        {
+            _crawlSession = new CrawlSession();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
