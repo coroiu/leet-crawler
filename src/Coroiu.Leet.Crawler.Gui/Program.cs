50f6d104f3e022fb7cc2f0203479b995e92b9e7e
Andreas Coroiu <andreas.coroiu@gmail.com>
2020-01-26 12:47:49 +0100

    Create solution.

---
+++ src/Coroiu.Leet.Crawler.Gui/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coroiu.Leet.Crawler.Gui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
