ed58b06c5705bb7a1e58b213068b133df3e41aee
Andreas Coroiu <andreas.coroiu@gmail.com>
2020-01-26 21:33:48 +0100

    Implement uri extraction.

--- src/Coroiu.Leet.Crawler.Net/HtmlPage.cs
+++ src/Coroiu.Leet.Crawler.Net/HtmlPage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Coroiu.Leet.Crawler.Net
 {
@@ -11,12 +12,18 @@ namespace Coroiu.Leet.Crawler.Net
 
         public string Content { get; }
 
-        public IEnumerable<Uri> Uris => Enumerable.Empty<Uri>();
+        public IEnumerable<Uri> Uris => ExtractAnchors(Content);
 
         public HtmlPage(Uri uri, string content)
         {
             Uri = uri;
             Content = content;
+        }
+
+        private static IEnumerable<Uri> ExtractAnchors(string content)
+        {
+            return Regex.Matches(content, "href\\s*=\\s*\"(?<url>.*?)\"")
+                .Select(m => new Uri(m.Groups["url"].Value));
         }
     }
 }
--- test/XUnitTestProject1/HtmlPageTest.cs
+++ test/XUnitTestProject1/HtmlPageTest.cs
@@ -16,5 +16,27 @@ namespace Coroiu.Leet.Crawlet.Net.Test
 
             uris.Should().BeEmpty();
         }
+
+        [Fact]
+        public void Uris_AnAnchor_ReturnsUri()
+        {
+            var page = new HtmlPage(new Uri("fake://a"), "<a href=\"http://test.com\">test link</a>");
+
+            var uris = page.Uris;
+
+            uris.Should().HaveCount(1)
+                .And.Contain(new Uri("http://test.com"));
+        }
+
+        [Fact]
+        public void Uris_MultipleAnchors_ReturnsUris()
+        {
+            var page = new HtmlPage(new Uri("fake://a"), "<html><body><a href=\"http://test.com\">test link</a>asdasdasd<a href=\"http://test2.com\">test link2</a></body><html>");
+
+            var uris = page.Uris;
+
+            uris.Should().HaveCount(2)
+                .And.Contain(new[] { new Uri("http://test.com"), new Uri("http://test2.com") });
+        }
     }
 }
