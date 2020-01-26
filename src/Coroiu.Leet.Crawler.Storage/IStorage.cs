abdf6afef2514c3e0815068aa3c5f05b9db55951
Andreas Coroiu <andreas.coroiu@gmail.com>
2020-01-26 14:59:57 +0100

    Add storage library.

---
+++ src/Coroiu.Leet.Crawler.Storage/IStorage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Coroiu.Leet.Crawler.Storage
+{
+    public interface IStorage
+    {
+        Task Save(Uri uri, string content);
+    }
+}
